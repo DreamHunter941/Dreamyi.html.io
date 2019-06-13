ea3ea602c2977132b84e563601714e8c807528b4
Dreamyi <994220227@qq.com>
2019-06-13 22:46:20 +0800

    1.0版本

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function showListDetail(){
+    document.getElementById("shopClass_Detail").className="shopClass_Detail show"
+    document.getElementById("shopBanner").className="shopBanner fl hide"
+    document.getElementById("user").className="user fl hide"
+}
+
+function hideListDetail(){
+    document.getElementById("shopClass_Detail").className="shopClass_Detail hide"
+    document.getElementById("shopBanner").className="shopBanner fl show"
+    document.getElementById("user").className="user fl show"
+}
