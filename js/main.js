31d87bd4348307c60955cccbe700433144da5c00
DreamHunter941 <51404342+DreamHunter941@users.noreply.github.com>
2019-06-17 20:02:19 +0800

    Add files via upload
    更新轮播图js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,42 @@ function hideListDetail(){
     document.getElementById("shopBanner").className="shopBanner fl show"
     document.getElementById("user").className="user fl show"
 }
+var i = 0;
+var imgbox = document.getElementById('imgbox').getElementsByClassName('bannerImg');
+var bannerImgPoint =document.getElementById('bannerImgPoint').getElementsByClassName('point_btn')
+function changeimg(){
+    
+    if (i<7){
+ 
+        imgbox[i].style.cssText = "opacity: 0;";
+        bannerImgPoint[i].className = "point_btn"
+        i++
+        imgbox[i].style.cssText = "opacity: 1;";
+        bannerImgPoint[i].className = "point_btn point_btn_active"
+        console.log (i);
+        
+    }
+    else{
+        imgbox[i].style.cssText = "opacity: 0;";
+        bannerImgPoint[i].className = "point_btn"
+        i = 0
+        console.log (i);
+        imgbox[i].style.cssText = "opacity: 1;";
+        bannerImgPoint[i].className = "point_btn point_btn_active"
+    }
+
+
+}
+
+function focusPoint(pointSet){
+    imgbox[i].style.cssText = "opacity: 0;";
+    bannerImgPoint[i].className = "point_btn"
+    i = pointSet
+    imgbox[i].style.cssText = "opacity: 1;";
+    bannerImgPoint[i].className = "point_btn point_btn_active"
+
+
+}
+
+
+setInterval(changeimg,2000)
